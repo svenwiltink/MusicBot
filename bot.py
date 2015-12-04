2bc0dce12747937938822a2920922dfd9563576a
swiltink <swiltink@transip.nl>
2015-12-04 16:57:55 +0100

    initial commit

---
+++ bot.py
@@ -0,0 +1,137 @@
+from twisted.words.protocols import irc
+from twisted.internet import ssl, reactor, protocol
+from twisted.python import log
+
+import os
+import time
+import sys
+import getpass
+
+class MessageLogger:
+    """
+    An independent logger class (because separation of application
+    and protocol logic is a good thing).
+    """
+    def __init__(self, file):
+        self.file = file
+
+    def log(self, message):
+        """Write a message to the file."""
+        timestamp = time.strftime("[%H:%M:%S]", time.localtime(time.time()))
+        self.file.write('%s %s\n' % (timestamp, message))
+        self.file.flush()
+
+    def close(self):
+        self.file.close()
+
+
+class MusicBot(irc.IRCClient):
+    """A logging IRC bot."""
+
+    commandPrefix = "!music"
+
+    def __init__(self):
+        self.commandHandler = CommandHandler(CommandManager())
+
+    def connectionMade(self):
+        irc.IRCClient.connectionMade(self)
+        self.logger = MessageLogger(open(self.factory.filename, "a"))
+        self.logger.log("[connected at %s]" %
+                        time.asctime(time.localtime(time.time())))
+
+    def connectionLost(self, reason):
+        irc.IRCClient.connectionLost(self, reason)
+        self.logger.log("[disconnected at %s]" %
+                        time.asctime(time.localtime(time.time())))
+        self.logger.close()
+
+    # callbacks for events
+
+    def signedOn(self):
+        """Called when bot has succesfully signed on to server."""
+        self.join(self.factory.channel)
+
+    def privmsg(self, user, channel, msg):
+        """This will get called when the bot receives a message."""
+        user = user.split('!', 1)[0]
+        self.logger.log("<%s> %s" % (user, msg))
+
+        if channel == self.factory.channel:
+            if str.startswith(msg, self.commandPrefix):
+                print "command prefix found!\n"
+                words = msg.split(" ")
+                commandName = "help"
+                if len(words) > 1:
+                    commandName = words[1]
+
+                arguments = words[2::]
+                output = self.commandHandler.handleCommand(self, user, commandName, arguments)
+                if output is not None:
+                    self.sendMessage(output)
+
+    def registerCommand(self, command):
+        self.commandHandler.getCommandManager().addCommand(command)
+
+    def sendMessage(self, message):
+        print "sending message"
+        print message
+        lines = message.split("\n")
+        for line in lines:
+                self.say(self.factory.channel, line)
+
+    def getCommandHandler(self):
+        return self.commandHandler
+
+
+
+
+class LogBotFactory(protocol.ClientFactory):
+    """A factory for LogBots.
+
+    A new protocol instance will be created each time we connect to the server.
+    """
+
+    def __init__(self, channel, nickname, password, filename):
+        self.channel = channel
+        self.filename = filename
+        self.nickname = nickname
+        self.password = password
+
+    def buildProtocol(self, addr):
+        p = MusicBot()
+        p.registerCommand(HelpCommand())
+        p.registerCommand(NextCommand())
+        p.registerCommand(PrevCommand())
+        p.registerCommand(CurrentCommand())
+        p.registerCommand(VolUpCommand())
+        p.registerCommand(VolDownCommand())
+        p.factory = self
+        p.nickname = self.nickname
+        p.password = self.password
+        return p
+
+    def clientConnectionLost(self, connector, reason):
+        """If we get disconnected, reconnect to server."""
+        connector.connect()
+
+    def clientConnectionFailed(self, connector, reason):
+        print "connection failed:", reason
+        reactor.stop()
+
+
+if __name__ == '__main__':
+    from command import CommandManager, CommandHandler, HelpCommand, NextCommand, PrevCommand, CurrentCommand, VolUpCommand, VolDownCommand
+    # initialize logging
+    log.startLogging(sys.stdout)
+
+    password = getpass.getpass()
+    # create factory protocol and application
+    f = LogBotFactory(sys.argv[1], sys.argv[2], password, sys.argv[3])
+
+    # connect factory to this host and port
+    reactor.connectSSL("znc.transip.us", 6697, f, ssl.ClientContextFactory())
+
+    # run bot
+    reactor.run()
+
+spotifyScript = os.path.realpath(os.path.dirname(__file__)) + "/spotify.sh"
---
+++ command.py
@@ -0,0 +1,114 @@
+import bot
+import subprocess
+import os
+class CommandManager():
+
+    commands = {}
+
+    def addCommand(self, command):
+        if command.getName() in self.commands:
+            raise AttributeError("command with name " + command.getName() + " is already registered")
+        self.commands[command.getName()] = command
+
+    def getCommand(self, name):
+        if name in self.commands:
+            return self.commands[name]
+        return None
+
+    def getAvailableCommands(self):
+        return self.commands.keys()
+
+class CommandHandler:
+
+    whitelist = [
+        'Sven',
+        'sGoen',
+        'Tom',
+        'Tim',
+        'Roos',
+        'hvandermeulen',
+        'Justin',
+        'sebastien',
+    ]
+
+    def __init__(self, commandManager):
+        self.commandManager = commandManager
+
+    def handleCommand(self, bot, user, commandName, arguments):
+        if user not in self.whitelist:
+            return
+        print "handeling command " + commandName
+        command = self.commandManager.getCommand(commandName)
+        if command is None:
+            return "Command not found"
+        return command.execute(bot, user, arguments)
+
+    def getCommandManager(self):
+        return self.commandManager
+
+
+class Command(object):
+
+    name = 'help'
+
+    def execute(self, bot, user, params):
+        return "not implemented"
+
+    def getName(self):
+        return self.name
+
+
+class HelpCommand(Command):
+
+    name = 'help'
+    def execute(self, bot, user, params):
+        helpText = "Available commands: "
+        commands = bot.getCommandHandler().getCommandManager().getAvailableCommands()
+        helpText += ", ".join(commands)
+
+        bot.sendMessage(helpText)
+
+
+class SpotifyCommand(Command):
+
+    def executeSpotifyCommand(self, command):
+        return subprocess.check_output([bot.spotifyScript, command])
+
+
+class NextCommand(SpotifyCommand):
+
+    name = 'next'
+
+    def execute(self, bot, user, params):
+        self.executeSpotifyCommand('next')
+        return "skipping song"
+
+
+class PrevCommand(SpotifyCommand):
+
+    name = 'prev'
+
+    def execute(self, bot, user, params):
+        self.executeSpotifyCommand('prev')
+        return "playing previous song"
+
+
+class CurrentCommand(SpotifyCommand):
+    name = 'current'
+
+    def execute(self, bot, user, params):
+        return self.executeSpotifyCommand('current')
+
+
+class VolUpCommand(Command):
+    name = 'vol++'
+
+    def execute(self, bot, user, params):
+        os.system("amixer -D pulse sset Master 10%+ >/dev/null")
+
+
+class VolDownCommand(Command):
+    name = 'vol--'
+
+    def execute(self, bot, user, params):
+        os.system("amixer -D pulse sset Master 10%- >/dev/null")
